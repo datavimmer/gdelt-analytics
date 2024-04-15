bc4c19d5251af612360a8b60e5d560977da06b76
datavimmer <yauheni.azaranka@gmail.com>
2024-04-15 20:56:02 +0200

    Added terraform configuration

---
+++ terraform/infra/variables.tf
@@ -0,0 +1,40 @@
+variable "credentials" {
+  description = "My Credentials"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "GDELT analytics"
+  default     = "1006817309523"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "europe-central2"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "EU"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "gdelt_dwh"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "gdelt_raw_data_bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
